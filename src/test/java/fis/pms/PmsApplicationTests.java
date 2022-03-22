ae8389c186e3c7a496e3738ebb8b0b000b298ec8
digda-5624 <hsg0208@naver.com>
2022-03-22 11:24:38 +0900

    [init] 초기화 작업진행

---
+++ src/test/java/fis/pms/PmsApplicationTests.java
@@ -0,0 +1,13 @@
+package fis.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
