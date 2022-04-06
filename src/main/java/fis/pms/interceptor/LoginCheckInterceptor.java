3298f183df66ca30b589653ebf3a918c4f31354b
Bum <sbl133@naver.com>
2022-04-06 15:45:59 +0900

    [fix] 현승구의 망각

--- src/main/java/fis/pms/interceptor/LoginCheckInterceptor.java
+++ src/main/java/fis/pms/interceptor/LoginCheckInterceptor.java
@@ -8,7 +8,6 @@ import javax.security.auth.login.LoginException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.Arrays;
 
 @Slf4j
 public class LoginCheckInterceptor implements HandlerInterceptor {
